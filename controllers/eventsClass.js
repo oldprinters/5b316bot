e7f2122347ec02e740fdd13846e5977b6f1b27a0
oldprinters <zakaz@oldprinters.ru>
2022-12-15 21:56:30 +0300

    начало событий

---
+++ controllers/eventsClass.js
@@ -0,0 +1,16 @@
+import { call_q } from '../config/query.js'
+import BaseName from './basename.js'
+//eventsClass.js класс работает с событиями (каникулы, экскурсии и т.п.)
+//каждое событие имеет двту и время начала и окончания. Дата окончания включена в событие.
+
+class EventsClass extends BaseName {
+    class
+    constructor() {
+        super('events_class')
+        // this.class = class
+    }
+    //------------------------------------
+    async searchEvents( class_id)
+}
+
+export default EventsClass
