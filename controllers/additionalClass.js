94d92e5bc2c63bcb66adcce7610cb330c215dff0
oldprinters <zakaz@oldprinters.ru>
2022-12-26 09:39:02 +0300

    d0

--- controllers/additionalClass.js
+++ controllers/additionalClass.js
@@ -44,6 +44,7 @@ class AdditionalClass extends BaseName {
             AND a.active = 1
             ORDER BY bn.name
         `
+        console.log(sql)
         return await call_q(sql, 'getListLessons')
     }
     //-------------------------------
