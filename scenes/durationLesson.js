9b96eecf0d6e86f760679af912fcf53bd8229a19
oldprinters <zakaz@oldprinters.ru>
2026-08-30 14:42:40 +0300

    исправлено создание класса

--- scenes/durationLesson.js
+++ scenes/durationLesson.js
@@ -1,7 +1,6 @@
-import {Telegraf, Markup, Scenes, session} from "telegraf"
+import { Scenes } from "telegraf"
 import MyClass from '../controllers/classes.js'
-import {createNewClassMenu, queryYesNoMenu, selectRoleMenu} from '../keyboards/keyboards.js'
-import { sanitizeInput } from '../utils.js'
+import { selectRoleMenu } from '../keyboards/keyboards.js'
 
 const durationLesson = new Scenes.BaseScene('DURATION_LESSON')
 //-----------------------------
@@ -9,30 +8,26 @@ durationLesson.enter(async ctx => {
     await ctx.reply('Укажите продолжительность занятия в минутах:')
 })
 //-----------------------------
-durationLesson.help( ctx => {
-    ctx.replyWithHTML('<b><u>HELP</u></b>\nУкажите продолжительность урока <b>в минутах</b>, она необходима для расчета времени окончания занятия.\n'+
+durationLesson.help( async ctx => {
+    await ctx.replyWithHTML('<b><u>HELP</u></b>\nУкажите продолжительность урока <b>в минутах</b>, она необходима для расчета времени окончания занятия.\n'+
                         'Только число.\n'+
                         'К примеру: для детских садов можно поставить "540" :-), что составляет 9 часов, а для студентов "90" - пара.')
 })
 //---------------------------------------------
-durationLesson.start( ctx => {ctx.scene.enter('SELECT_ACTION')})
+durationLesson.start( async ctx => {await ctx.scene.enter('SELECT_ACTION')})
 //-------------------------------------------------
 durationLesson.command('games', async ctx => {
-    ctx.scene.enter('GAMES')
+    await ctx.scene.enter('GAMES')
 })
 //---------------------------------------------
 durationLesson.hears(/^\d{1,3}$/, async ctx => {
-    if(/^\d{1,3}$/.test(ctx.message.text)){
-        const myClass = new MyClass(ctx)
-        await myClass.init()
-        ctx.session.duration = sanitizeInput(parseInt(ctx.message.text))
-        ctx.session.class_id = await myClass.appendClass(ctx.session.className, ctx.session.duration)
-        ctx.session.isAdmin = 1
-        await ctx.reply(`Код класса "${ctx.session.className}" записан.`)
-    ctx.reply('Выберите роль в меню:', selectRoleMenu())
-    } else {
-        ctx.reply('Укажите продолжительность занятия в минутах цифрами (45, 90 и т.д.):')
-    }
+    const myClass = new MyClass(ctx)
+    await myClass.init()
+    ctx.session.duration = parseInt(ctx.message.text)
+    ctx.session.class_id = await myClass.appendClass(ctx.session.className, ctx.session.duration)
+    ctx.session.isAdmin = 1
+    await ctx.reply(`Код класса "${ctx.session.className}" записан.`)
+    await ctx.reply('Выберите роль в меню:', selectRoleMenu())
 })
 //-----------------------------------------
 const saveCUR = async (ctx) => {
@@ -42,7 +37,7 @@ const saveCUR = async (ctx) => {
     const classList = await myClass.searchClasses()
     ctx.session.i = classList.length - 1
     ctx.session.classList = classList   //сделать отдельную сцену инициализации
-    ctx.scene.enter('SET_TIMES_UR')
+    await ctx.scene.enter('SET_TIMES_UR')
 }
 //-----------------------------------------
 durationLesson.action('studentRole', async ctx => {
